0e9f7b014bf7bfbb7dbca6712d21dbca50b1ea80
Evan <evan@chequer.io>
2020-01-10 19:14:14 +0900

    Remove debug code

--- Snowflake.Data/Logger/SFLoggerFactory.cs
+++ Snowflake.Data/Logger/SFLoggerFactory.cs
@@ -2,19 +2,13 @@
  * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
  */
 
-// using log4net;
-
-using System;
-
 namespace Snowflake.Data.Log
 {
     class SFLoggerFactory
     {
         public static SFLogger GetLogger<T>()
         {
-            // ILog logger = LogManager.GetLogger(typeof(T));
-            // return new Log4netImpl(logger);
-            throw new Exception();
+            return new Log4netImpl();
         }
     }
 }
